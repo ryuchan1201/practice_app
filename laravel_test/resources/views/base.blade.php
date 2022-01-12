16fdda84adce9086be1915033b9da5cd21adf9b1
ryugo <ryuchansenpai1201@gmail.com>
2022-01-12 16:51:54 +0900

    “簡単なアプリ”

---
+++ laravel_test/resources/views/base.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Laravel 8 & MySQL CRUD Tutorial</title>
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+  <div class="container">
+    @yield('main')
+  </div>
+  <script src="{{ asset('js/app.js') }}" type="text/js"></script>
+</body>
+</html>
